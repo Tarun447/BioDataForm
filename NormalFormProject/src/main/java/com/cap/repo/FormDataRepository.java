ce4f090d55c4384f7370ee23eaaa73b3a102b201
Tarun Kumar Nanda <72005790+Tarun447@users.noreply.github.com>
2022-07-13 21:56:36 +0530

    Delete NormalFormProject directory

--- NormalFormProject/src/main/java/com/cap/repo/FormDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cap.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cap.enity.FormData;
-
-public interface FormDataRepository extends MongoRepository<FormData, String> {
-
-}
